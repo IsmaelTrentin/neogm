687a74e3771b6dff73e2d0c08e31243b4f959eed
Ismael Trentin <ismael.trentin@samtrevano.ch>
2023-02-09 19:48:15 +0100

    refactor types

--- src/@types/model.ts
+++ src/@types/model.ts
@@ -1,29 +1,49 @@
-import { GetLabelsTypeFromSchema, GetPropertiesTypeFromSchema } from './utils';
+import {
+  GetLabelsTypeFromSchema,
+  GetPropertiesTypeFromSchema,
+  Properties,
+} from './utils';
+import { RelationshipSchema, Schemas } from '.';
 
 import { Node } from 'neo4j-driver';
-import { Schema } from '.';
+import { QueryNodeResult } from './node';
 
-export interface ModelObject<S extends Schema> {
+export interface ModelObject<S extends Schemas> {
   labels: GetLabelsTypeFromSchema<S>;
   properties: GetPropertiesTypeFromSchema<S>;
 }
 
-export interface Model<S extends Schema> extends ModelObject<S> {
+export interface Model<S extends Schemas> extends ModelObject<S> {
   toString(varName?: string): string;
   toObject(): ModelObject<S>;
   save(varName?: string): Promise<ModelObject<S>['properties']>;
 }
 
-export type ModelFactory = <S extends Schema>(
+export type PropertiesKeysObject<P extends Properties = Properties> = {
+  [Property in keyof P]: keyof P;
+};
+
+type CreateFunction<S extends Schemas> = (
+  properties: GetPropertiesTypeFromSchema<S>
+) => Model<S>;
+type MatchFunction<S extends Schemas> = (
+  filter?: Partial<GetPropertiesTypeFromSchema<S>>
+) => Promise<QueryNodeResult<S>[]>;
+type QueryBuilderFunction<S extends Schemas> = (
+  labels: GetLabelsTypeFromSchema<S>,
+  keys: PropertiesKeysObject<GetPropertiesTypeFromSchema<S>>
+) => string;
+type QueryFunction<S extends Schemas> = (
+  builder: QueryBuilderFunction<S>,
+  varNames?: string[]
+) => Promise<QueryNodeResult<S>[]>;
+
+export type ModelFactory = <S extends Schemas>(
   name: string,
   schema: S,
   unique?: boolean
 ) => {
-  create: (properties: GetPropertiesTypeFromSchema<S>) => Model<S>;
-  all(): Promise<QueryNodeResult<S>[]>;
+  create: CreateFunction<S>;
+  match: MatchFunction<S>;
+  query: QueryFunction<S>;
 };
-
-interface QueryNodeResult<S extends Schema> extends Node {
-  labels: GetLabelsTypeFromSchema<S>;
-  properties: GetPropertiesTypeFromSchema<S>;
-}
