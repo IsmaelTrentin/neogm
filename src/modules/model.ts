faeb11febff504b4375fe43715508f15ca232e5c
Ismael Trentin <ismael.trentin@samtrevano.ch>
2023-02-09 19:49:22 +0100

    refactor + add `query` method

--- src/modules/model.ts
+++ src/modules/model.ts
@@ -1,44 +1,66 @@
-import { GetPropertiesTypeFromSchema, ModelFactory } from '../@types';
+import {
+  GetPropertiesTypeFromSchema,
+  ModelFactory,
+  PropertiesKeysObject,
+} from '../@types';
 
 import { node } from './node';
 import { session } from './connect';
 
-export const model: ModelFactory = (name, schema, unique = true) => ({
-  create: properties => {
-    // TODO: fix ts(2322) @ ModelFactory <S...
-    return {
-      labels: schema.labels as any, //
-      properties,
-      toString: (varName = 'n') => {
-        return node.buildNode(schema.labels, properties, varName);
-      },
-      _modelName: name,
-      toObject: () => {
-        return {
-          labels: schema.labels as any, //
-          properties,
-        };
-      },
-      save: async (varName: string = 'n') => {
-        const built = node.buildNode(schema.labels, properties, varName);
-        const createMode = unique ? 'MERGE' : 'CREATE';
-        const result = await session
-          .get()
-          .run<typeof properties>(
-            `${createMode} ${built} RETURN ${varName}`,
-            properties
-          );
-        return result.records[0].get(varName);
-      },
-    };
-  },
-  all: async () => {
-    const built = node.buildNode(schema.labels, {});
-    const result = await session
-      .get()
-      .run<GetPropertiesTypeFromSchema<typeof schema>>(
-        `MATCH ${built} RETURN n`
-      );
-    return result.records.map(r => r.get('n'));
-  },
-});
+export const model: ModelFactory = (name, schema, unique = true) => {
+  type Props = GetPropertiesTypeFromSchema<typeof schema>;
+
+  const { schemaProperties } = schema;
+  // TODO: fix ts(2322) @ ModelFactory <S...
+  const labels = schema.labels;
+
+  return {
+    create: properties => {
+      return {
+        labels: labels, //
+        properties,
+        toString: (varName = 'n') => {
+          return node.buildNode(labels, properties, varName);
+        },
+        _modelName: name,
+        toObject: () => {
+          return {
+            labels, //
+            properties,
+          };
+        },
+        save: async (varName: string = 'n') => {
+          const built = node.buildNode(labels, properties, varName);
+          const createMode = unique ? 'MERGE' : 'CREATE';
+          const result = await session
+            .get()
+            .run<typeof properties>(
+              `${createMode} ${built} RETURN ${varName}`,
+              properties
+            );
+          return result.records[0].get(varName);
+        },
+      };
+    },
+    match: async (filter: Partial<Props> = {}) => {
+      const built = node.buildNode(labels, filter);
+      const result = await session
+        .get()
+        .run<Props>(`MATCH ${built} RETURN n`, filter);
+      return result.records.map(r => r.get('n'));
+    },
+    query: async (builder, varNames = ['n']) => {
+      const keys = Object.keys(schemaProperties) as (keyof Props)[]; // or as keyof SchemaProperties ?
+      let keysObject = {} as PropertiesKeysObject<Props>;
+      for (const k of keys) {
+        keysObject[k] = k;
+      }
+      const query = builder(labels, keysObject);
+      const result = await session
+        .get()
+        .run<GetPropertiesTypeFromSchema<typeof schema>>(query);
+
+      return result.records as any;
+    },
+  };
+};
