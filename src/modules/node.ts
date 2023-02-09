14381696414bc1e99225f50006d7d4810d6a85c0
Ismael Trentin <ismael.trentin@samtrevano.ch>
2023-02-09 19:49:29 +0100

    refactor

--- src/modules/node.ts
+++ src/modules/node.ts
@@ -1,9 +1,14 @@
 import { Properties } from '../@types';
 
 export const node = {
-  buildNode(labels: string[], properties: Properties, varName: string = 'n') {
+  buildNode(
+    labels: string[] | readonly string[],
+    properties: Properties,
+    varName: string = 'n'
+  ) {
     const keys = Object.keys(properties).map(k => `${k}: \$${k}`);
     const props = keys.length === 0 ? '' : ` {${keys.join(',')}}`;
-    return `(${varName.trim()}:${labels.join(':')}${props})`;
+    const nodeStr = `(${varName.trim()}:${labels.join(':')}${props})`;
+    return nodeStr;
   },
 };
