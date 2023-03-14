e89e5cf5387a713a37bdf85a82794314cd194659
Ismael Trentin <ismael.trentin@samtrevano.ch>
2023-03-14 03:04:15 +0100

    refactor: add comments

--- src/test/schemas/actor.ts
+++ src/test/schemas/actor.ts
@@ -5,8 +5,13 @@ export const actorSchema = neogm.schema.node<ActorLabels, ActorProps>(
   ['Actor'],
   {
     name: {
+      // remove type?? no need to track it since
+      // there is no migration needed to be pushed
+      // agains the db.
       type: String,
       default: 'asd',
+      // relationship: true  (makes rest never)
+      //               false (makes reldata never)
     },
   }
 );
